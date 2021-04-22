23302a661e29152ccffc61176bd80fc11d2947de
lytico <lytico@users.sourceforge.net>
2021-04-22 02:49:50 +0200

    Core.csproj: Gtk: first drafts for Font-Handling

--- src/Core/src/Platform/Linux/FontExtensions.cs
+++ src/Core/src/Platform/Linux/FontExtensions.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using Gtk;
 
 namespace Microsoft.Maui
@@ -11,8 +12,20 @@ namespace Microsoft.Maui
 			=> it.StyleContext.GetFont(it.StateFlags);
 #pragma warning restore 612
 
+		/// <summary>
+		/// size in points
+		/// <seealso cref="https://developer.gnome.org/pygtk/stable/class-pangofontdescription.html#method-pangofontdescription--set-size"/>
+		/// the size of a font description is specified in pango units.
+		/// There are <see cref="Pango.Scale.PangoScale"/> pango units in one device unit (the device unit is a point for font sizes).
+		/// </summary>
+		/// <param name="it"></param>
+		/// <returns></returns>
 		public static double GetSize(this Pango.FontDescription it)
 			=> it.Size / Pango.Scale.PangoScale;
+		
+		public static Pango.FontFamily GetPangoFontFamily(this Widget it)
+			=> it.FontMap.Families.First();
+
 
 	}
 
