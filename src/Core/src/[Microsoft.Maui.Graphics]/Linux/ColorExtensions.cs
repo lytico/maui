ad656878df7ed31fc5a75b79eb1894d533eb00fd
lytico <lytico@users.sourceforge.net>
2021-04-23 00:09:08 +0200

    Core.csproj: Gtk: add nullchecks in ColorExtensions.cs

--- src/Core/src/[Microsoft.Maui.Graphics]/Linux/ColorExtensions.cs
+++ src/Core/src/[Microsoft.Maui.Graphics]/Linux/ColorExtensions.cs
@@ -14,25 +14,25 @@
 		}
 
 		public static Gdk.RGBA ToGdkRgba(this Color color)
-			=> new() { Red = color.Red, Green = color.Green, Blue = color.Blue, Alpha = color.Alpha };
+			=> color == default ? default : new() { Red = color.Red, Green = color.Green, Blue = color.Blue, Alpha = color.Alpha };
 
 		public static Gdk.Color ToGdkColor(this Gdk.RGBA color)
 			=> new((byte)(color.Red * 255), (byte)(color.Green * 255), (byte)(color.Blue * 255));
-		
-		public static Color ToColor(this Gdk.Color color, float opacity = 255) 
+
+		public static Color ToColor(this Gdk.Color color, float opacity = 255)
 			=> new(color.Red, color.Green, color.Blue, opacity);
 
 		public static Color ToColor(this Gdk.RGBA color)
 			=> new((byte)(color.Red * 255), (byte)(color.Green * 255), (byte)(color.Blue * 255), (byte)(color.Alpha * 255));
 
-		public static Cairo.Color ToCairoColor(this Color col)
-			=> new(col.Red, col.Green, col.Blue, col.Alpha);
+		public static Cairo.Color ToCairoColor(this Color color)
+			=> color == default ? default : new(color.Red, color.Green, color.Blue, color.Alpha);
 
 		public static Cairo.Color ToCairoColor(this Gdk.RGBA color)
 			=> new(color.Red, color.Green, color.Blue, color.Alpha);
 
 		public static Gdk.Color ToGdkColor(this Color color)
-			=> new((byte)(color.Red * 255), (byte)(color.Green * 255), (byte)(color.Blue * 255));
+			=> color == default ? default : new((byte)(color.Red * 255), (byte)(color.Green * 255), (byte)(color.Blue * 255));
 
 		public static Color ToColor(this Gdk.Color color)
 			=> new((float)color.Red / (float)ushort.MaxValue, (float)color.Green / (float)ushort.MaxValue, (float)color.Blue / (float)ushort.MaxValue);
