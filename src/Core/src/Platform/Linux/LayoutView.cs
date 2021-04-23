c4f7b20109ba9f65c3f35c99fb30b129a211edee
lytico <lytico@users.sourceforge.net>
2021-04-23 06:12:54 +0200

    Core.csproj: Gtk: LayoutView.cs: add debug code

--- src/Core/src/Platform/Linux/LayoutView.cs
+++ src/Core/src/Platform/Linux/LayoutView.cs
@@ -1,10 +1,22 @@
-﻿using Gdk;
+﻿using Cairo;
 using Gtk;
+using Rectangle = Gdk.Rectangle;
 
 namespace Microsoft.Maui
 {
 	public class LayoutView : Fixed
 	{
+#if DEBUG
+		protected override bool OnDrawn(Context cr)
+		{
+			var r = base.OnDrawn(cr);
+
+			cr.Save();
+			cr.ShowText($"{nameof(LayoutView)}");
+			cr.Restore();
+			return r;
+		}
+#endif
 		protected override void OnAdjustSizeRequest(Orientation orientation, out int minimum_size, out int natural_size)
 		{
 			base.OnAdjustSizeRequest(orientation, out minimum_size, out natural_size);
@@ -13,6 +25,9 @@ namespace Microsoft.Maui
 		protected override void OnSizeAllocated(Rectangle allocation)
 		{
 			base.OnSizeAllocated(allocation);
+	
 		}
+		
+		
 	}
 }
