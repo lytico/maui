943c7e7c4a337ea3ca0c0977017db6d8f094ac2f
lytico <lytico@users.sourceforge.net>
2021-05-13 18:00:35 +0200

    Compatibility.Gtk.csproj: introduce

---
+++ src/Compatibility/Core/src/Gtk/AppHostBuilderExtensions.cs
@@ -0,0 +1,17 @@
+using Microsoft.Maui.Hosting;
+
+namespace Microsoft.Maui.Controls.Compatibility
+{
+
+	public static class AppHostBuilderExtensions
+	{
+
+		public static IAppHostBuilder UseCompatibilityRenderers(this IAppHostBuilder builder)
+		{
+
+			return builder;
+		}
+
+	}
+
+}
