108ff7a872135ba49116ae7dcbb71e71348e5e11
lytico <lytico@users.sourceforge.net>
2021-05-15 05:14:40 +0200

    Core.csproj Gtk : enhance PickerHandler.Linux.cs

--- src/Core/src/Handlers/Picker/PickerHandler.Linux.cs
+++ src/Core/src/Handlers/Picker/PickerHandler.Linux.cs
@@ -2,8 +2,10 @@
 
 namespace Microsoft.Maui.Handlers
 {
+
 	public partial class PickerHandler : ViewHandler<IPicker, ComboBox>
 	{
+
 		protected override ComboBox CreateNativeView()
 		{
 			return new ComboBox();
@@ -18,18 +20,21 @@ namespace Microsoft.Maui.Handlers
 		[MissingMapper]
 		public static void MapCharacterSpacing(PickerHandler handler, IPicker view) { }
 
-		[MissingMapper]
-		public static void MapFont(PickerHandler handler, IPicker view) { }
+		public static void MapFont(PickerHandler handler, IPicker view)
+		{
+			handler.MapFont(view);
+
+		}
 
 		[MissingMapper]
 		public static void MapTextColor(PickerHandler handler, IPicker view) { }
-		
+
 		[MissingMapper]
 		public static void MapReload(PickerHandler handler, IPicker picker) { }
-		
+
 		[MissingMapper]
 		public static void MapHorizontalTextAlignment(PickerHandler handler, IPicker view) { }
 
-
 	}
+
 }
