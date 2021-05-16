765d44328f56515e990ffdd0302fa7bb470de5cd
lytico <lytico@users.sourceforge.net>
2021-05-16 17:58:37 +0200

    Core.csproj Gtk : LabelHandler.Linux.cs: prepare some code to fix linebreakmode for long lines

--- src/Core/src/Handlers/Label/LabelHandler.Linux.cs
+++ src/Core/src/Handlers/Label/LabelHandler.Linux.cs
@@ -1,4 +1,6 @@
-﻿using Gtk;
+﻿using System;
+using Gtk;
+using Microsoft.Maui.Graphics;
 
 namespace Microsoft.Maui.Handlers
 {
@@ -9,7 +11,78 @@ namespace Microsoft.Maui.Handlers
 		// https://developer.gnome.org/gtk3/stable/GtkLabel.html
 		protected override Label CreateNativeView()
 		{
-			return new Label();
+			return new Label()
+			{
+				// Hexpand = true,
+				// MaxWidthChars = 1,
+			};
+		}
+
+		private SizeRequest _perfectSize;
+
+		private bool _perfectSizeValid;
+		// private bool _allocated = false;
+
+		private SizeRequest GetPerfectSize(int widthConstraint = -1)
+		{
+			if (NativeView is not { } nativeView)
+				return default;
+
+			nativeView.Layout.Width = Pango.Units.FromPixels(widthConstraint);
+			nativeView.Layout.GetPixelSize(out int w, out int h);
+
+			return new SizeRequest(new Size(w, h));
+		}
+
+		public virtual Size GetDesiredSize1(double widthConstraint, double heightConstraint)
+		{
+			if (VirtualView is not { } virtualView)
+				return default;
+
+			// if (!_allocated && PlatformHelper.GetGTKPlatform() == GTKPlatform.Windows)
+			// {
+			// 	return default(SizeRequest);
+			// }
+
+			if (!_perfectSizeValid)
+			{
+				_perfectSize = GetPerfectSize();
+				_perfectSize.Minimum = new Size(Math.Min(10, _perfectSize.Request.Width), _perfectSize.Request.Height);
+				_perfectSizeValid = true;
+			}
+
+			var widthFits = widthConstraint >= _perfectSize.Request.Width;
+			var heightFits = heightConstraint >= _perfectSize.Request.Height;
+
+			if (widthFits && heightFits)
+				return _perfectSize;
+
+			var result = GetPerfectSize((int)widthConstraint);
+			var tinyWidth = Math.Min(10, result.Request.Width);
+			result.Minimum = new Size(tinyWidth, result.Request.Height);
+
+			if (widthFits || virtualView.LineBreakMode == LineBreakMode.NoWrap)
+			{
+				return new SizeRequest(
+					new Size(result.Request.Width, _perfectSize.Request.Height),
+					new Size(result.Minimum.Width, _perfectSize.Request.Height));
+			}
+
+			bool containerIsNotInfinitelyWide = !double.IsInfinity(widthConstraint);
+
+			if (containerIsNotInfinitelyWide)
+			{
+				bool textCouldHaveWrapped = virtualView.LineBreakMode == LineBreakMode.WordWrap || virtualView.LineBreakMode == LineBreakMode.CharacterWrap;
+				bool textExceedsContainer = result.Request.Width > widthConstraint;
+
+				if (textExceedsContainer || textCouldHaveWrapped)
+				{
+					var expandedWidth = Math.Max(tinyWidth, widthConstraint);
+					result.Request = new Size(expandedWidth, result.Request.Height);
+				}
+			}
+
+			return result;
 		}
 
 		public static void MapText(LabelHandler handler, ILabel label)
@@ -50,15 +123,11 @@ namespace Microsoft.Maui.Handlers
 
 		[MissingMapper]
 		public static void MapCharacterSpacing(LabelHandler handler, ILabel label)
-		{
-			
-		}
+		{ }
 
 		[MissingMapper]
 		public static void MapTextDecorations(LabelHandler handler, ILabel label)
-		{
-			
-		}
+		{ }
 
 		[MissingMapper]
 		public static void MapLineHeight(LabelHandler handler, ILabel label)
