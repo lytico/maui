db0efcfd957a78d2dd6cfd3c5188c3e880940b04
lytico <lytico@users.sourceforge.net>
2021-05-20 03:34:01 +0200

    Core.csproj Gtk : Label: try fix sizing problem

--- src/Core/src/Handlers/Label/LabelHandler.Linux.cs
+++ src/Core/src/Handlers/Label/LabelHandler.Linux.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using System.Runtime.InteropServices.ComTypes;
 using Gtk;
 using Microsoft.Maui.Graphics;
+using Microsoft.Maui.Graphics.Native.Gtk;
+using Pango;
 
 namespace Microsoft.Maui.Handlers
 {
 
 	public partial class LabelHandler : ViewHandler<ILabel, Label>
 	{
+
+		private static Microsoft.Maui.Graphics.Native.Gtk.TextLayout? _textLayout;
+
+		public Microsoft.Maui.Graphics.Native.Gtk.TextLayout SharedTextLayout => _textLayout ??= new Microsoft.Maui.Graphics.Native.Gtk.TextLayout(
+			Microsoft.Maui.Graphics.Native.Gtk.NativeGraphicsService.Instance.SharedContext)
+		{
+			HeightForWidth = true
+		};
 
 		// https://developer.gnome.org/gtk3/stable/GtkLabel.html
 		protected override Label CreateNativeView()
@@ -15,18 +26,49 @@ namespace Microsoft.Maui.Handlers
 			{
 				// Hexpand = true,
 				// MaxWidthChars = 1,
+				LineWrap = true,
+				// nativeView.Lines = 10;
+				Halign = Align.Fill,
+				Xalign = 0,
+				MaxWidthChars = 1
 			};
 		}
 
 		// note:fix that problem by using
 		// Microsoft.Maui.Graphics.Native.Gtk - NativeGraphicsService.GetStringSize
 
-		public virtual Size GetDesiredSize1(double widthConstraint, double heightConstraint)
+		public override Size GetDesiredSize(double widthConstraint, double heightConstraint)
 		{
+			if (NativeView is not { } nativeView)
+				return default;
+
 			if (VirtualView is not { } virtualView)
 				return default;
 
-			return base.GetDesiredSize(widthConstraint, heightConstraint);
+			var res = base.GetDesiredSize(widthConstraint, heightConstraint);
+
+			lock (SharedTextLayout)
+			{
+				SharedTextLayout.FontFamily = virtualView.Font.FontFamily;
+				SharedTextLayout.TextFlow = TextFlow.ClipBounds;
+				SharedTextLayout.PangoFontSize = virtualView.Font.FontSize.ScaledToPango();
+				SharedTextLayout.HorizontalAlignment = virtualView.HorizontalTextAlignment.GetHorizontalAlignment();
+				SharedTextLayout.LineBreakMode = virtualView.LineBreakMode.GetLineBreakMode();
+			}
+
+			var (width,height) = SharedTextLayout.GetPixelSize(NativeView.Text, double.IsInfinity(widthConstraint)?-1:widthConstraint);
+			var inkRect = new Pango.Rectangle();
+			var logicalRect = new Pango.Rectangle();
+			nativeView.Layout.GetLineReadonly(0).GetExtents(ref inkRect, ref logicalRect);
+
+			// nativeView.SetSizeRequest((int)ts.Width,(int)ts.Height);
+			// nativeView.QueueResize();
+			// nativeView.QueueComputeExpand();
+			// res.Width = ts.Width + nativeView.MarginStart + nativeView.MarginEnd;
+			// res.Height = ts.Height + nativeView.MarginTop + nativeView.MarginBottom;
+			return new Size(width,height);
+
+			
 		}
 
 		public static void MapText(LabelHandler handler, ILabel label)
