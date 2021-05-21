34209a0bfaf04fca2d3a0e385b4918f207ee837d
lytico <lytico@users.sourceforge.net>
2021-05-21 04:52:11 +0200

    Core.csproj Gtk : enhance handlers

--- src/Core/src/Platform/Linux/HandlerExtensions.cs
+++ src/Core/src/Platform/Linux/HandlerExtensions.cs
@@ -3,8 +3,10 @@ using System;
 
 namespace Microsoft.Maui
 {
+
 	public static class HandlerExtensions
 	{
+
 		public static Widget ToNative(this IView view, IMauiContext context)
 		{
 			_ = view ?? throw new ArgumentNullException(nameof(view));
