b2f23b1fb6b1fe405fd54ac9037f24fb5bdd90cc
lytico <lytico@users.sourceforge.net>
2021-07-16 19:05:32 +0200

    Essentials.csproj: Gtk implement Clipboard.Gtk.cs PlatformSetTextAsync, PlatformHasText, PlatformGetTextAsync (kudos to https://github.com/xamarin/Essentials/pull/1290 @Bobface)

--- src/Essentials/src/Clipboard/Clipboard.Gtk.cs
+++ src/Essentials/src/Clipboard/Clipboard.Gtk.cs
@@ -2,21 +2,43 @@ using System.Threading.Tasks;
 
 namespace Microsoft.Maui.Essentials
 {
+
 	public static partial class Clipboard
 	{
+
+		static readonly Gdk.Atom clipboardAtom = Gdk.Atom.Intern("CLIPBOARD", false);
+
 		static Task PlatformSetTextAsync(string text)
-			=> throw ExceptionUtils.NotSupportedOrImplementedException;
+		{
+			var clipboard = Gtk.Clipboard.Get(clipboardAtom);
+			clipboard.Text = text;
+
+			return Task.FromResult(0);
+		}
 
 		static bool PlatformHasText
-			=> throw ExceptionUtils.NotSupportedOrImplementedException;
+		{
+			get
+			{
+				var clipboard = Gtk.Clipboard.Get(clipboardAtom);
+
+				return clipboard.WaitIsTextAvailable();
+			}
+		}
 
 		static Task<string> PlatformGetTextAsync()
-			=> throw ExceptionUtils.NotSupportedOrImplementedException;
+		{
+			var clipboard = Gtk.Clipboard.Get(clipboardAtom);
+
+			return Task.FromResult(clipboard.WaitForText());
+		}
 
 		static void StartClipboardListeners()
 			=> throw ExceptionUtils.NotSupportedOrImplementedException;
 
 		static void StopClipboardListeners()
 			=> throw ExceptionUtils.NotSupportedOrImplementedException;
+
 	}
+
 }
