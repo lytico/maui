a625eb01db0a9b04324b3769964f50564ac49ae0
lytico <lytico@users.sourceforge.net>
2021-08-18 20:52:38 +0200

    Core.csproj Gtk : LabelExtensions.cs: handle TextType (stub)  & Controls.Core.cproj: add Label.Gtk.cs

---
+++ src/Controls/src/Core/HandlerImpl/Label/Label.Gtk.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Maui.Handlers;
+
+namespace Microsoft.Maui.Controls
+{
+
+	public partial class Label
+	{
+
+		public static void RemapForControls()
+		{
+			IPropertyMapper<ILabel, LabelHandler> ControlsLabelMapper = new PropertyMapper<Label, LabelHandler>(LabelHandler.LabelMapper)
+			{
+				[nameof(TextType)] = MapTextType,
+				[nameof(Text)] = MapText,
+			};
+
+			LabelHandler.LabelMapper = ControlsLabelMapper;
+		}
+
+		public static void MapTextType(LabelHandler handler, Label label)
+		{
+			handler.NativeView?.UpdateText(label, label.TextType);
+		}
+
+		public static void MapText(LabelHandler handler, Label label)
+		{
+			handler.NativeView?.UpdateText(label, label.TextType);
+		}
+
+	}
+
+}
