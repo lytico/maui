9ec7cb6c1d0ac3a605cb707ddd301cd11c7f56e3
Derby Russell <DerbyRussell27@gmail.com>
2022-10-12 10:14:22 -0400

    Set GTK solution to only have GTK items

---
+++ src/Controls/samples-gtk/Controls.Sample.OnePage.GTK/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Graphics;
+
+namespace Maui.Controls.Sample
+{
+	public partial class MainPage : ContentPage
+	{
+		int count = 0;
+
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		private void OnCounterClicked(object sender, EventArgs e)
+		{
+			count++;
+
+			if (count == 1)
+				CounterBtn.Text = $"Clicked {count} time";
+			else
+				CounterBtn.Text = $"Clicked {count} times";
+		}
+	}
+}
