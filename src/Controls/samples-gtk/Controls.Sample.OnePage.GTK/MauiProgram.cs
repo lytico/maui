9b8f3c65538c4bd1a852f73ca5df2f297933536b
Derby Russell <DerbyRussell27@gmail.com>
2022-10-17 14:12:06 -0400

    Maui.Controls.Sample.OnePage.GTK builds with the help of GenerateXamlRunner project

--- src/Controls/samples-gtk/Controls.Sample.OnePage.GTK/MauiProgram.cs
+++ src/Controls/samples-gtk/Controls.Sample.OnePage.GTK/MauiProgram.cs
@@ -1,12 +1,25 @@
 ﻿using Microsoft.Maui;
 using Microsoft.Maui.Controls;
 using Microsoft.Maui.Controls.Hosting;
+using Microsoft.Maui.Controls.PlatformConfiguration;
 using Microsoft.Maui.Hosting;
 
 namespace Maui.Controls.Sample
 {
 	public static class MauiProgram
 	{
+		static void Main()
+		{
+			// Gtk.Application.Init();
+			// MauiProgram.CreateMauiApp();
+
+			// var app = new App();
+
+			// Gtk.Application.Run();
+
+			System.Diagnostics.Debug.WriteLine("Inside MauiProgram.Main.");
+		}
+
 		public static MauiApp CreateMauiApp() =>
 			MauiApp
 				.CreateBuilder()
@@ -16,7 +29,7 @@ namespace Maui.Controls.Sample
 
 	class App : Application
 	{
-		protected override Window CreateWindow(IActivationState activationState) =>
-			new Window(new MainPage());
+		//protected override Window CreateWindow(IActivationState activationState) =>
+		//	new Window(new MainPage());
 	}
 }
---
+++ src/Controls/tests/GenerateXamlRunner/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Maui.Controls.SourceGen;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Generate1File();
+GenerateAnotherFile();
+
+void Generate1File()
+{
+	var fileNameRoot = "MainPage.xaml";
+	var fileNameSourcePath = "D:\\Source\\maui\\src\\Controls\\samples-gtk\\Controls.Sample.OnePage.GTK";
+	var fullFileNamePath = Path.Combine(fileNameSourcePath, fileNameRoot);
+	var fileSource = File.ReadAllText(fullFileNamePath);
+	var projectOutputPath = "D:\\Source\\maui\\src\\Controls\\samples-gtk\\Controls.Sample.OnePage.GTK\\bin\\Debug\\net6.0-windows";
+	GenerateXaml.GenerateXamlCodeBehind(fileSource, fileNameRoot, fileNameSourcePath, fileNameRoot, projectOutputPath);
+}
+
+void GenerateAnotherFile()
+{
+	var fileNameRoot = "SandboxShell.xaml";
+	var fileNameSourcePath = "D:\\Source\\maui\\src\\Controls\\samples-gtk\\Controls.Sample.OnePage.GTK";
+	var fullFileNamePath = Path.Combine(fileNameSourcePath, fileNameRoot);
+	var fileSource = File.ReadAllText(fullFileNamePath);
+	var projectOutputPath = "D:\\Source\\maui\\src\\Controls\\samples-gtk\\Controls.Sample.OnePage.GTK\\bin\\Debug\\net6.0-windows";
+	GenerateXaml.GenerateXamlCodeBehind(fileSource, fileNameRoot, fileNameSourcePath, fileNameRoot, projectOutputPath);
+}
