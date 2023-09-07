f37c15750f18304e10bbeacccb6f311513691c93
lytico <lytico@users.sourceforge.net>
2023-09-07 18:29:45 +0200

    BlazorWebView.Samples BlazorGtkApp: add old sampe again

--- src/BlazorWebView/samples/BlazorGtkApp/Program.cs
+++ src/BlazorWebView/samples/BlazorGtkApp/Program.cs
@@ -2,9 +2,11 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Gtk;
+using GtkSharp.BlazorWebKit;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebView.Gtk;
 using WebViewAppShared;
+using GtkWebViewManager = Microsoft.AspNetCore.Components.WebView.Gtk.GtkWebViewManager;
 
 AppState _appState = new();
 
@@ -41,14 +43,43 @@ blazorWebView1.Services = services1.BuildServiceProvider();
 blazorWebView1.RootComponents.Add<BlazorGtkApp.Main>("#app");
 blazorWebView1.RootComponents.RegisterForJavaScript<MyDynamicComponent>("my-dynamic-root-component");
 
+
 var customFilesBlazorWebView = new BlazorWebView();
 customFilesBlazorWebView.HostPage = @"wwwroot\customindex.html";
 customFilesBlazorWebView.Services = services2.BuildServiceProvider();
 customFilesBlazorWebView.RootComponents.Add<BlazorGtkApp.Main>("#app");
 
-window.Add(blazorWebView1);
+var nb = new Gtk.Notebook();
+var tab1 = nb.AppendPage(blazorWebView1, new Label(nameof(blazorWebView1)));
+var tab2 = nb.AppendPage(customFilesBlazorWebView, new Label(nameof(customFilesBlazorWebView)));
 
 
+// this is the old stuff
+var serviceProvider = new ServiceCollection()
+	.AddBlazorWebViewOptions(new BlazorWebViewOptions() 
+	{ 
+		RootComponent = typeof(BlazorGtkApp.Main),
+		HostPath = "wwwroot/index.html"
+	})
+	.AddLogging((lb) =>
+	{
+		lb.AddSimpleConsole(options =>
+			{
+				//options.ColorBehavior = Microsoft.Extensions.Logging.Console.LoggerColorBehavior.Disabled;
+				//options.IncludeScopes = false;
+				//options.SingleLine = true;
+				options.TimestampFormat = "hh:mm:ss ";
+			})
+			.SetMinimumLevel(LogLevel.Information);
+	})
+	.BuildServiceProvider();
+
+var webViewRaw = new WebKit.WebView();
+var manager = GtkWebViewManager.NewForWebView(webViewRaw, serviceProvider);
+var tab3 = nb.AppendPage(webViewRaw, new Label(nameof(webViewRaw)));
+
+
+window.Add(nb);
 window.ShowAll();
 
 Application.Run();
