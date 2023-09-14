5336483815eeab1c77bc57f87d351e7383790700
lytico <lytico@users.sourceforge.net>
2023-09-14 03:38:22 +0200

    BlazorGtkApp.csproj Program.cs: remove debug code

--- src/BlazorWebView/samples/BlazorGtkApp/Program.cs
+++ src/BlazorWebView/samples/BlazorGtkApp/Program.cs
@@ -1,13 +1,10 @@
 ﻿using System.IO;
 using BlazorGtkApp;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using Gtk;
-using GtkSharp.BlazorWebKit;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebView.Gtk;
 using WebViewAppShared;
-using GtkWebViewManager = Microsoft.AspNetCore.Components.WebView.Gtk.GtkWebViewManager;
 
 #pragma warning disable CS0162 // Unreachable code detected
 
@@ -41,51 +38,20 @@ services2.AddBlazorWebViewDeveloperTools();
 services2.AddSingleton<AppState>(_appState);
 
 var nb = new Gtk.Notebook();
-if (true)
-{
-// this is the old stuff
-	var serviceProvider = new ServiceCollection()
-		.AddBlazorWebViewOptions(new BlazorWebViewOptions() { 
-			RootComponent = typeof(BlazorGtkApp.Main), 
-			HostPath = Path.Combine("wwwroot", "index.html") })
-		.AddLogging((lb) =>
-		{
-			lb.AddSimpleConsole(options =>
-				{
-					//options.ColorBehavior = Microsoft.Extensions.Logging.Console.LoggerColorBehavior.Disabled;
-					//options.IncludeScopes = false;
-					//options.SingleLine = true;
-					options.TimestampFormat = "hh:mm:ss ";
-				})
-				.SetMinimumLevel(LogLevel.Information);
-		})
-		.BuildServiceProvider();
 
-	var webViewRaw = new WebKit.WebView();
-	webViewRaw.Settings.EnableDeveloperExtras = true;
-	var manager = GtkWebViewManager.NewForWebView(webViewRaw, serviceProvider);
-	var tab3 = nb.AppendPage(webViewRaw, new Label(nameof(webViewRaw)));
-}
+var blazorWebView1 = new BlazorWebView();
+blazorWebView1.HostPage = Path.Combine("wwwroot", "index.html");
+blazorWebView1.Services = services1.BuildServiceProvider();
+blazorWebView1.RootComponents.Add<BlazorGtkApp.Main>("#app");
+blazorWebView1.RootComponents.RegisterForJavaScript<MyDynamicComponent>("my-dynamic-root-component");
+var tab1 = nb.AppendPage(blazorWebView1, new Label(nameof(blazorWebView1)));
 
-if (true)
-{
-	var blazorWebView1 = new BlazorWebView();
-	blazorWebView1.HostPage = Path.Combine("wwwroot", "index.html");
-	blazorWebView1.Services = services1.BuildServiceProvider();
-	blazorWebView1.RootComponents.Add<BlazorGtkApp.Main>("#app");
-	blazorWebView1.RootComponents.RegisterForJavaScript<MyDynamicComponent>("my-dynamic-root-component");
-	var tab1 = nb.AppendPage(blazorWebView1, new Label(nameof(blazorWebView1)));
-}
+var customFilesBlazorWebView = new CustomFilesBlazorWebView();
+customFilesBlazorWebView.HostPage = Path.Combine("wwwroot", "customindex.html");
+customFilesBlazorWebView.Services = services2.BuildServiceProvider();
+customFilesBlazorWebView.RootComponents.Add<BlazorGtkApp.Main>("#app");
 
-if (false)
-{
-	var customFilesBlazorWebView = new BlazorWebView();
-	customFilesBlazorWebView.HostPage = Path.Combine("wwwroot", "customindex.html");
-	customFilesBlazorWebView.Services = services2.BuildServiceProvider();
-	customFilesBlazorWebView.RootComponents.Add<BlazorGtkApp.Main>("#app");
-
-	var tab2 = nb.AppendPage(customFilesBlazorWebView, new Label(nameof(customFilesBlazorWebView)));
-}
+var tab2 = nb.AppendPage(customFilesBlazorWebView, new Label(nameof(customFilesBlazorWebView)));
 
 
 window.Add(nb);
