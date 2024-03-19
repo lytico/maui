6b63071f633a09ffeca045c617f82fedee981a72
lytico <lytico@users.sourceforge.net>
2024-03-19 03:05:24 +0100

    [Gtk] Core.csproj ImageView: introduce Measure (has logical error!)

--- src/Core/src/Platform/Gtk/ImageView.cs
+++ src/Core/src/Platform/Gtk/ImageView.cs
@@ -2,6 +2,7 @@ using System;
 using System.Net.Mime;
 using Cairo;
 using Gtk;
+using Microsoft.Maui.Graphics;
 using Microsoft.Maui.Graphics.Platform.Gtk;
 using Rectangle = Gdk.Rectangle;
 
@@ -46,6 +47,66 @@ namespace Microsoft.Maui.Platform
 			}
 		}
 
+		public static Size Measure(Aspect aspect, Size desiredSize, double widthConstraint, double heightConstraint)
+		{
+			double desiredAspect = desiredSize.Width / desiredSize.Height;
+			double constraintAspect = widthConstraint / heightConstraint;
+
+			double desiredWidth = desiredSize.Width;
+			double desiredHeight = desiredSize.Height;
+
+			if (desiredWidth == 0 || desiredHeight == 0)
+				return new SizeRequest(new Size(0, 0));
+
+			double width = desiredWidth;
+			double height = desiredHeight;
+
+			if (constraintAspect > desiredAspect)
+			{
+				// constraint area is proportionally wider than image
+				switch (aspect)
+				{
+					case Aspect.AspectFit:
+					case Aspect.AspectFill:
+						height = Math.Min(desiredHeight, heightConstraint);
+						width = desiredWidth * (height / desiredHeight);
+
+						break;
+					case Aspect.Fill:
+						width = Math.Min(desiredWidth, widthConstraint);
+						height = desiredHeight * (width / desiredWidth);
+
+						break;
+				}
+			}
+			else if (constraintAspect < desiredAspect)
+			{
+				// constraint area is proportionally taller than image
+				switch (aspect)
+				{
+					case Aspect.AspectFit:
+					case Aspect.AspectFill:
+						width = Math.Min(desiredWidth, widthConstraint);
+						height = desiredHeight * (width / desiredWidth);
+
+						break;
+					case Aspect.Fill:
+						height = Math.Min(desiredHeight, heightConstraint);
+						width = desiredWidth * (height / desiredHeight);
+
+						break;
+				}
+			}
+			else
+			{
+				// constraint area is same aspect as image
+				width = Math.Min(desiredWidth, widthConstraint);
+				height = desiredHeight * (width / desiredWidth);
+			}
+
+			return new Size(width, height);
+		}
+
 		protected override bool OnDrawn(Context cr)
 		{
 			var a = Allocation;
@@ -62,14 +123,11 @@ namespace Microsoft.Maui.Platform
 			if (a.Width == 1 && a.Height == 1 && a.X == -1 && a.Y == -1) // the allocation coordinates on reallocation
 				return base.OnDrawn(cr);
 
-			var xalign = 0;
-			var yalign = 0;
-			int x = (int)((a.Width - (float)image.Width) * xalign);
-			int y = (int)((a.Height - (float)image.Height) * yalign);
-			if (x < 0) x = 0;
-			if (y < 0) y = 0;
+			var imageSize = Measure(Aspect, new Size(image.Width, image.Height), a.Width, a.Height);
+			var x = (a.Width - imageSize.Width) / 2;
+			var y = (a.Height - imageSize.Height) / 2;
 
-			cr.DrawPixbuf(image, x, y, a.Width, a.Height);
+			cr.DrawPixbuf(image, x, y, imageSize.Width, imageSize.Height);
 
 			return base.OnDrawn(cr);
 		}
@@ -106,7 +164,8 @@ namespace Microsoft.Maui.Platform
 			if (Image is { })
 			{
 
-				minimum_height = Image.Height * width / Image.Width;
+				var size = Measure(Aspect, new Size(Image.Width, Image.Height), width, (double)Image.Height * width / Image.Width);
+				minimum_height = (int)size.Height;
 				natural_height = Math.Max(Image.Height, minimum_height);
 			}
 		}
@@ -117,7 +176,8 @@ namespace Microsoft.Maui.Platform
 
 			if (Image is { })
 			{
-				minimum_width = Image.Width * height / Image.Height;
+				var size = Measure(Aspect, new Size(Image.Width, Image.Height), (double)Image.Width * height / Image.Height, height);
+				minimum_width = (int)size.Width;
 				natural_width = Math.Max(Image.Width, minimum_width);
 
 			}
